e2e91b46a64a74a53f0d8f5693a4a3d51d88fff5
panlatent <panlatent@gmail.com>
2019-04-24 16:20:56 +0800

    Version 0.2.0-alpha.6

--- src/Plugin.php
+++ src/Plugin.php
@@ -53,7 +53,7 @@ class Plugin extends \craft\base\Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '0.2.0-alpha.5';
+    public $schemaVersion = '0.2.0-alpha.6';
 
     /**
      * @inheritdoc
